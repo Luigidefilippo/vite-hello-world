c76ef52118ab571220b372f5ef5e4e9ee0819f8c
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-12 15:48:43 +0200

    finish exercise

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppJumbo",
+    data() {
+        return {
+            title: "Benevenuto in VITE + VUE!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+    <img class="banner-img" src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="Milano">
+</template>
+
+<style>
+    .banner-img {
+        width: 100%;
+        height: 300px;
+        object-fit: contain;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "Piacere di conoscerti"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style></style>
